73eec279db65b936479a6e197533b94aabd2e3c2
LIMA\kimyo <kimyoulim1016@gmail.com>
2022-11-30 18:48:10 +0900

    미니프로젝트 시작!

---
+++ src/main/java/com/lima/battleofmyeongnyang/BattleofMyeongnyangApplication.java
@@ -0,0 +1,13 @@
+package com.lima.battleofmyeongnyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleofMyeongnyangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattleofMyeongnyangApplication.class, args);
+    }
+
+}
