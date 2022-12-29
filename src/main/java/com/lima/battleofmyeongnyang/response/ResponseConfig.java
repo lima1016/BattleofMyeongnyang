fb5d8338ad11498ab99c2776c06fdfc5e2bdbf45
kimyoulim <kimyoulim1016@gmail.com>
2022-12-29 21:36:59 +0900

    리턴 형식 정리하는중....

--- src/main/java/com/lima/battleofmyeongnyang/response/ResponseConfig.java
+++ src/main/java/com/lima/battleofmyeongnyang/response/ResponseConfig.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,11 +14,19 @@ public class ResponseConfig {
 
   private String code = "HELLO-00000";
   private String msg = "SUCCESS";
-  private Object response = "";
+  private Object response;
 
   public ResponseConfig getResponse(Object response) {
-    // 다시보기
+    // response 형식
     return (ResponseConfig) (this.response = response);
+  }
+
+  // LIM: 으아아아아 Json 형식이였으면좋겠어~!!
+  public Map getSuccessCode() {
+    Map<String, Object> resultMap = new HashMap<>();
+    resultMap.put(code, getCode());
+    resultMap.put(msg, getMsg());
+    return  resultMap;
   }
   public static ResponseConfig isHelloEmpty() {
     return new ResponseConfig();
