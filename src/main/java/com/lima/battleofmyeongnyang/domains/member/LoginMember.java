f7675293e209c8c55144da7e5afb5cf2884c0522
kimyoulim <kimyoulim1016@gmail.com>
2023-03-09 22:15:33 +0900

    redis 저장 set

--- src/main/java/com/lima/battleofmyeongnyang/domains/member/LoginMember.java
+++ src/main/java/com/lima/battleofmyeongnyang/domains/member/LoginMember.java
@@ -2,12 +2,15 @@ package com.lima.battleofmyeongnyang.domains.member;
 
 import io.hypersistence.utils.hibernate.type.basic.Inet;
 import lombok.Data;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Data
 public class LoginMember {
 
   private String loginId;
   private Inet loginIp;
+  // 로그인 정보 저장용
+  private RedisTemplate redisTemplate;
 
   private static class Singleton {
     private static final LoginMember INSTANCE = new LoginMember();
@@ -15,5 +18,8 @@ public class LoginMember {
 
   public static LoginMember getInstance() {
     return Singleton.INSTANCE;
+  }
+
+  public void setRedisTemplate(String loginId, Inet loginIp) {
   }
 }
